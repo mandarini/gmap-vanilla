d4aabaa5f924284eacf0bd35a7f294fcb8c3718f
Katerina Skroumpelou <sk.katherine@gmail.com>
2019-11-19 11:44:29 +0200

    autocomplete and window

--- app/scripts/map.ts
+++ app/scripts/map.ts
@@ -2,6 +2,7 @@
 import {} from "google-maps";
 import * as styledMap from "./styledMap";
 import { listenForDrawing } from "./drawing";
+import { placesSearch } from "./placesSearch";
 
 import { customGradient } from "./gradient";
 import { mapNumber } from "./mapNumber";
@@ -54,50 +55,7 @@ export function FunWithMaps(map: google.maps.Map) {
     document.getElementById("place-search")
   );
 
-  const autocomplete_input: HTMLInputElement = document.getElementById(
-    "place-input"
-  ) as HTMLInputElement;
-
-  const autocomplete = new google.maps.places.Autocomplete(autocomplete_input);
-  autocomplete.bindTo("bounds", map);
-  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
-  const marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
-  autocomplete.addListener("place_changed", function() {
-    marker.setVisible(false);
-    const place = autocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
-
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-
-    let address = "";
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] &&
-          place.address_components[0].short_name) ||
-          "",
-        (place.address_components[1] &&
-          place.address_components[1].short_name) ||
-          "",
-        (place.address_components[2] &&
-          place.address_components[2].short_name) ||
-          ""
-      ].join(" ");
-    }
-  });
-
+  placesSearch(map);
   listenForDrawing(map);
   loadAllMarkers(map);
   loadHeatmapData();
